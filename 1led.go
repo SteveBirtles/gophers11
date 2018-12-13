ef289de5cff5974dea12314d567739337b33dcbe
Raspberry Pi Gopher <pi@farnborough.ac.uk>
2018-12-13 10:23:08 +0000

    Fixed for Pi

--- 1led.go
+++ 1led.go
@@ -4,18 +4,17 @@ import (
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/firmata"
-	"time"
 )
 
 func main() {
 
-	firmataAdaptor := firmata.NewAdaptor("COM3")
+	firmataAdaptor := firmata.NewAdaptor("/dev/ttyACM0")
 
 	led := gpio.NewDirectPinDriver(firmataAdaptor, "2")
 
 	work := func() {
 		led.On()
-	})
+	}
 
 	robot := gobot.NewRobot("bot",
 		[]gobot.Connection{firmataAdaptor},
